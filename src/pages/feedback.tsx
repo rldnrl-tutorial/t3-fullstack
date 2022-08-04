1e53833c4dbf2509cd82831a501ef7b3d1894eaf
rldnrl <devpark1002@gmail.com>
2022-08-04 10:40:50 +0900

    refactor: refactor feedback page

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -1,14 +1,5 @@
 import FeedbackTable from "@/Feedback/components/FeedbackTable";
-import { Feedback } from "@/libs/types/feedback";
-import Loading from "@/libs/ui/Loading";
-import { trpc } from "@/utils/trpc";
 
 export default function FeedbackPage() {
-  const feedbacksQuery = trpc.useQuery(["feedback.all"]);
-
-  if (feedbacksQuery.isLoading) {
-    return <Loading />;
-  }
-
-  return <FeedbackTable feedbacks={feedbacksQuery.data as Feedback[]} />;
+  return <FeedbackTable />;
 }
